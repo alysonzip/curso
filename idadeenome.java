42b886c7bcae406e81e2842f85de0bb79db2f991
tuskzip <116466288+alysonzip@users.noreply.github.com>
2026-09-22 19:59:23 -0300

    Create idadeenome.java

---
+++ idadeenome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nome;
+        int idade;
+        System.out.print("Informe seu nome: ");
+        nome = scanner.nextLine();
+        System.out.print("Informe sua idade: ");
+        idade=scanner.nextInt();
+        if(idade<0){
+            System.out.println(nome + " sua idade e invalida");
+        }
+        else if (idade<18){
+            System.out.println(nome + " e adolescente");
+        }
+        else if(idade>=18){
+            System.out.println(nome + " e adulto");
+        }
+        scanner.close();
+    }
+}
