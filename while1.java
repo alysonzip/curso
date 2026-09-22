b553e098a04e30434ef599c5c5a3919c6b61763f
tuskzip <116466288+alysonzip@users.noreply.github.com>
2026-09-22 20:26:23 -0300

    Create while1.java

---
+++ while1.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int cont=0;
+        while(cont<5){
+            System.out.println("Boa noite");
+            cont++;
+        }
+        
+    }
+}
