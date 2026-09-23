6360e45fb839d3c5d2e573c049e27ba4d86ce164
tuskzip <116466288+alysonzip@users.noreply.github.com>
2026-09-22 21:15:54 -0300

    Create dowhile1.java

---
+++ dowhile1.java
@@ -0,0 +1,11 @@
+
+class Main {
+    public static void main(String[] args) {
+        int cont = 0;
+        do{
+            System.out.println(cont);
+            cont++;
+        } while (cont < 4);
+
+    }
+}
